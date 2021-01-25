00df216d324de6ac82589b9d7e183665c9fdd24e
unknown <katdiemin@gmail.com>
2021-01-25 12:04:30 -0500

    campaign class

--- client/campaign/campaign.ts
+++ client/campaign/campaign.ts
@@ -1,7 +1,7 @@
 export class Campaign {
     campaignID: string = '';
     campaignName: string = '';
-    DM: string = '';
+    DM: number = -1;
     players: number[] = [];
     notes: number[] = [];
     constructor(id: string, campaignName: string, DM: string, players: number[], notes: number[]){};
