8eef4caaec1aac4cc309c68986a984552d286455
KirstenWood <kirstenhope98@gmail.com>
2021-02-06 18:51:20 -0500

    unauthorized

--- client/unauthorized.component.tsx
+++ client/unauthorized.component.tsx
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
+import styles from './global-styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>UNAUTHORIZED</Text>
+      <Text style={styles.logo}>UNAUTHORIZED</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
